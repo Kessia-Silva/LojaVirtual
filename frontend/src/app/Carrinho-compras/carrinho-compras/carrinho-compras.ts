071b7553336e946027c6a05d30e6731d6eab4619
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-12-02 09:42:57 -0300

    Componente Carrinho de compras

---
+++ frontend/src/app/Carrinho-compras/carrinho-compras/carrinho-compras.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho-compras',
+  imports: [],
+  templateUrl: './carrinho-compras.html',
+  styleUrl: './carrinho-compras.scss',
+})
+export class CarrinhoCompras {
+
+}
