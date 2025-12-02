ea280588f6acc841f36f6279cef6d5309e12edb5
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-12-02 09:57:15 -0300

    Área de Produto

---
+++ frontend/src/app/produto-info/produto-info/produto-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-info',
+  imports: [],
+  templateUrl: './produto-info.html',
+  styleUrl: './produto-info.scss',
+})
+export class ProdutoInfo {
+
+}
